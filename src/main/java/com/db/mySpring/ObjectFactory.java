1ab9f87e2ab19273e617c4d9f99030ac61c86ffb
bsevgeny@gmail.com <papakita2009>
2017-08-28 13:07:31 +0300

    Inject example lab

--- src/main/java/com/db/mySpring/ObjectFactory.java
+++ src/main/java/com/db/mySpring/ObjectFactory.java
@@ -1,10 +1,17 @@
 package com.db.mySpring;
 
+import lombok.SneakyThrows;
+import org.reflections.Reflections;
+
+import java.util.Set;
+
 /**
  * Created by Evegeny on 28/08/2017.
  */
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
+    private Config config = new JavaConfig();
+    private Reflections scanner = new Reflections("com.db");
 
     public static ObjectFactory getInstance() {
         return ourInstance;
@@ -13,7 +20,37 @@ public class ObjectFactory {
     private ObjectFactory() {
     }
 
+    @SneakyThrows
     public <T> T createObject(Class<T> type) {
-        return null;
+        type = resolveImpl(type);
+        T t = type.newInstance();
+
+
+        return t;
+    }
+
+    private <T> Class<T> resolveImpl(Class<T> type) {
+        if (type.isInterface()) {
+            Class<T> implClass = config.getImplClass(type);
+            if (implClass == null) {
+                Set<Class<? extends T>> classes = scanner.getSubTypesOf(type);
+                if (classes.size() != 1) {
+                    throw new RuntimeException(type + " should have only one impl, otherwise update config");
+                }
+                implClass = (Class<T>) classes.iterator().next();
+            }
+            type = implClass;
+        }
+        return type;
     }
 }
+
+
+
+
+
+
+
+
+
+
